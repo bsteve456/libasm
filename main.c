6e76eb485eb9cd6a0d64a8b867a7dda4c9b4130f
Steve-alan BALEBA BALEBA <stbaleba@e1r11p23.42.fr>
2020-02-08 18:23:54 +0100

    add strcpy

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: stbaleba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/08 10:23:23 by stbaleba          #+#    #+#             */
-/*   Updated: 2020/02/08 17:17:34 by stbaleba         ###   ########.fr       */
+/*   Updated: 2020/02/08 18:10:25 by stbaleba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <assert.h>
 
 size_t	ft_strlen(const char *s);
+char	*strcpy(char *dst, const char *src);
 
 int main()
 {
@@ -23,7 +24,7 @@ int main()
 	char *s = "acedd";
 	printf("%ld\n", i = ft_strlen(s));
 	printf("%ld\n", i = strlen(s));
-
+	printf("%s\n", ft_strcpy("abc", "ade"));
 //	assert(i == 1 && "not good");
 //	printf("%ld\n", ft_strlen("a"));
 }
--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -1,31 +1,33 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    Makefile                                           :+:      :+:    :+:    #
+#    ft_strcpy.s                                        :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: stbaleba <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/02/08 10:07:36 by stbaleba          #+#    #+#              #
-#    Updated: 2020/02/08 17:23:55 by stbaleba         ###   ########.fr        #
+#    Created: 2020/02/08 17:24:48 by stbaleba          #+#    #+#              #
+#    Updated: 2020/02/08 18:12:37 by stbaleba         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-CC=nasm
-CFLAG=-fmacho64
-NAME=libasm.a
-FILE=srcs/ft_strlen.s \
-	srcs/ft_strcpy.s
+		section		.text
+			extern		_ft_strlen
+			global		_ft_strcpy
 
-OBJ=$(FILE:.s=.o)
 
-all :$(NAME)
-
-$(NAME): $(OBJ)
-		ar rc $(NAME) $(OBJ)
-.s.o: $(FILE)
-	$(CC) $(CFLAG) $<
-clean:
-		rm -rf  ./srcs/*.o
-fclean: clean
-		rm -rf $(NAME)
-re: fclean all
+_ft_strcpy:
+		xor rdx, rdx
+		xor rcx, rcx
+		xor rax, rax
+		push rdi
+		mov  rdi, rsi
+		call _ft_strlen
+		pop rdi
+		while:
+				mov BYTE[rdi + rdx], BYTE[rsi + rcx]
+				inc rdx
+				inc rcx
+				dec rax
+				cmp rax, 0
+				jne while
+				ret
